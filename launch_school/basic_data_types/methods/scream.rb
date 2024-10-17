9caca62729da04f0ba1345c8f24d2e2f0e6bacf3
Roelof Wobben <r.wobben@home.nl>
2024-10-17 19:18:05 +0200

    first challenges solved

---
+++ launch_school/basic_data_types/methods/scream.rb
@@ -0,0 +1,20 @@
+# What will the following code print to the screen?
+
+def scream(words)
+  words = words + "!!!!"
+  return
+  puts words
+end
+
+scream("Yippeee")
+
+# nothing 
+
+#1) Edit the method definition in exercise #4 so that it does print words on the screen. 2) What does it return now?
+
+def scream2(words)
+  words = words + "!!!!"
+  puts words
+end
+
+scream2("Yippeee")
